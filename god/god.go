1881e5eb1fddedef31b7c4f3c37d27ab227d653c
zero <627838134@qq.com>
2019-05-20 19:54:45 +0800

    初始化仓库

---
+++ god/god.go
@@ -0,0 +1,5 @@
+package god
+
+func Atob() string {
+	return "Atob"
+}
---
+++ main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"god"
+)
+
+func main() {
+	fmt.Println("god of zero")
+	fmt.Println(god.Atob())
+}
