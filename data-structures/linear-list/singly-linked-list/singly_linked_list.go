044afadd8167ad6e0bbc48e24dbef7d8bafb94c8
zero <627838134@qq.com>
2019-06-12 15:40:59 +0800

    add singly linked list

--- data-structures/linear-list/singly-linked-list/singly_linked_list.go
+++ data-structures/linear-list/singly-linked-list/singly_linked_list.go
@@ -1,5 +1,10 @@
 package singly_linked_list
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Node struct {
 	data interface{}
 	next *Node
@@ -7,4 +12,128 @@ type Node struct {
 
 type SinglyLinkedList struct {
 	first *Node
+}
+
+func NewList() *SinglyLinkedList {
+	return new(SinglyLinkedList)
+}
+
+func (list *SinglyLinkedList) Length() int {
+	size := 0
+	p := list.first
+
+	for p != nil {
+		p = p.next
+		size++
+	}
+
+	return size
+}
+
+func (list *SinglyLinkedList) Get(i int) (interface{}, error) {
+	j := 0
+	p := list.first
+
+	if i < 0 || i > list.Length() {
+		return nil, errors.New("下标超出范围")
+	}
+
+	for p != nil && j < i-1 {
+		j++
+		p = p.next
+	}
+
+	return p.data, nil
+}
+
+func (list *SinglyLinkedList) Locate(v interface{}) (int, error) {
+	i := 0
+	p := list.first
+
+	for p != nil {
+		if p.data == v {
+			return i + 1, nil
+		}
+		i++
+		p = p.next
+	}
+
+	return -1, errors.New("元素不存在")
+}
+
+func (list *SinglyLinkedList) Insert(i int, x interface{}) error {
+
+	if i < 0 || i > list.Length()+1 {
+		return errors.New("下标超出范围")
+	}
+
+	if i == 1 {
+		list.first = &Node{x, list.first}
+	} else {
+		j := 0
+		p := list.first
+
+		for p != nil && j < i-2 {
+			p = p.next
+			j++
+		}
+
+		node := &Node{x, nil}
+
+		if p.next != nil {
+			node.next = p.next
+		}
+
+		p.next = node
+	}
+
+	return nil
+}
+
+func (list *SinglyLinkedList) Delete(i int) error {
+
+	if i < 0 || i > list.Length() {
+		return errors.New("下标超出范围")
+	}
+
+	if i == 1 {
+		list.first = list.first.next
+	} else {
+		j := 0
+		p := list.first
+
+		for p != nil && j < i-2 {
+			p = p.next
+			j++
+		}
+
+		p.next = p.next.next
+
+	}
+
+	return nil
+	// j := 0
+	// p := list.first
+
+	// if p != nil {
+	// 	if p.next != nil {
+	// 		p.next = p.next.next
+	// 	} else {
+	// 		p.next = nil
+	// 	}
+	// 	return nil
+	// } else {
+	// 	return errors.New("下标超出范围")
+	// }
+}
+
+func (list *SinglyLinkedList) Print() {
+	p := list.first
+	i := 1
+
+	for p != nil {
+		fmt.Printf("第%d位是：%v\n", i, p.data)
+		p = p.next
+		i++
+	}
 }
--- data-structures/linear-list/singly-linked-list/singly_linked_list.test.go
+++ data-structures/linear-list/singly-linked-list/singly_linked_list.test.go
@@ -3,5 +3,41 @@ package singly_linked_list
 import "fmt"
 
 func SinglyLinkedListTest() {
-	fmt.Println("SinglyLinkedListTest")
+	list := NewList()
+
+	list.Insert(1, 6)
+	list.Insert(2, 2)
+	list.Insert(3, 7)
+	list.Insert(4, 8)
+	list.Insert(5, 3)
+	list.Insert(6, 8)
+	list.Insert(7, 1)
+	list.Insert(8, 3)
+	list.Insert(9, 4)
+	list.Insert(1, 0)
+	list.Insert(6, 0)
+	list.Insert(12, 0)
+	list.Insert(-1, 0)
+	list.Insert(14, 0)
+
+	list.Delete(1)
+	list.Delete(5)
+	list.Delete(10)
+	list.Delete(-1)
+	list.Delete(10)
+
+	fmt.Println(list.Get(1))
+	fmt.Println(list.Get(5))
+	fmt.Println(list.Get(9))
+	fmt.Println(list.Get(-1))
+	fmt.Println(list.Get(10))
+
+	fmt.Println(list.Locate(6))
+	fmt.Println(list.Locate(3))
+	fmt.Println(list.Locate(4))
+	fmt.Println(list.Locate(9))
+
+	fmt.Println(list.Length())
+
+	list.Print()
 }
